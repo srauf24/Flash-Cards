8435b49cf3ebba813c2aa4d7356a05ca35cea739
Samee Rauf <samee.rauf@pmg.com>
2024-12-31 16:03:31 -0800

    created use state for card count & changed tab name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [cardCount, setcardCount] = useState(0);
+
+
   return (
     <div className="App">
       <header className="Header">
        <h1> Memon Vocabulary</h1>
-          <p> Lets practice your memoni  🍵</p>
+          <p className="Description"> Lets practice your memoni  🍵</p>
+            <p className="CardCount">Card Count: {cardCount}</p>
       </header>
     </div>
   );
